0029a0b49da45d47a9db33086eafd022796a6444
Ceegor <ceephone333@gmail.com>
2026-09-21 16:18:02 +0200

    Update published assembly version regression for v0.5.0

--- tests/PromptHelper.Tests/AuditDefectRegressionTests.cs
+++ tests/PromptHelper.Tests/AuditDefectRegressionTests.cs
@@ -462,13 +462,13 @@ public sealed class AuditDefectRegressionTests
     }
 
     [TestMethod]
-    public void PLH2004_Assembly_version_is_0_4_0()
+    public void PLH2004_Assembly_version_is_0_5_0()
     {
         var asm = typeof(PromptHelper.App).Assembly;
         var version = asm.GetName().Version;
         Assert.IsNotNull(version);
         Assert.AreEqual(0, version.Major);
-        Assert.AreEqual(4, version.Minor);
+        Assert.AreEqual(5, version.Minor);
         Assert.AreEqual(0, version.Build);
     }
 
