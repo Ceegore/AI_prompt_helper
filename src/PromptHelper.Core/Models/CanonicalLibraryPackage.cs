23c15c0c9bdc5177ed9064e9e38703cf0f0506c3
Ceegor <ceephone333@gmail.com>
2026-09-21 11:47:36 +0200

    Move CanonicalLibraryPackage.cs into cross-platform core

---
+++ src/PromptHelper.Core/Models/CanonicalLibraryPackage.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Security.Cryptography;
+using PromptHelper.Services;
+
+namespace PromptHelper.Models;
+
+public sealed record CanonicalLibraryPackage
+{
+    public LibraryDocument Document { get; }
+    public byte[] CanonicalBytes { get; }
+    public string Sha256Hex { get; }
+
+    internal CanonicalLibraryPackage(LibraryDocument document, byte[] canonicalBytes, string sha256Hex)
+    {
+        Document = document;
+        CanonicalBytes = canonicalBytes;
+        Sha256Hex = sha256Hex;
+    }
+
+    public static CanonicalLibraryPackage Create(LibraryDocument document)
+    {
+        ArgumentNullException.ThrowIfNull(document);
+        LibraryValidator.Validate(document);
+        LibraryDocument clone = LibraryDocumentCloner.Clone(document);
+        string json = LibraryJson.SerializeCanonical(clone);
+        byte[] bytes = StrictUtf8Text.Encode(json);
+        return new CanonicalLibraryPackage(
+            clone,
+            bytes,
+            Convert.ToHexStringLower(SHA256.HashData(bytes)));
+    }
+}
